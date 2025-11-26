b91b06877b1191d4e1a12a75ab64dc839085103d
Sen-CaPoo <broken.sen@gmail.com>
2025-11-26 15:28:57 +0800

    fix(migration): 將 IssueReports 表中的 RecordDate 欄位類型從 date 更新為 datetime2

---
+++ Migrations/20251124005540_ChangeRecordDateToDateTime2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClarityDesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeRecordDateToDateTime2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RecordDate",
+                table: "IssueReports",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RecordDate",
+                table: "IssueReports",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
